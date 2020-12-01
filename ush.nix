90c25854089566a330883818555acecbe2b63d85
Hunter Jones <hjones2199@gmail.com>
2020-12-01 14:06:30 -0600

    Switched to meson build system, added nix derivation

---
+++ ush.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  pname = "ush";
+  version = "0.71";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [ libedit ];
+
+  meta = with lib; {
+    description = "small personal shell";
+    homepage = "https://qirus.net";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ hjones2199 ];
+  };
+}
