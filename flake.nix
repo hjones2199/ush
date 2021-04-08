c6c2e150726c1bd32fba2b91897030228c99dd4e
Hunter Jones <hjones2199@gmail.com>
2021-04-07 21:00:41 -0500

    Add RISCV cross-compilation via nix

--- flake.nix
+++ flake.nix
@@ -6,26 +6,51 @@
   outputs = { self, nixpkgs }: {
     defaultPackage.x86_64-linux =
       with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation rec {
-        pname = "ush";
-        version = "0.72";
+        stdenv.mkDerivation rec {
+          pname = "ush";
+          version = "0.72";
 
-        src = self;
+          src = self;
 
-        nativeBuildInputs = [
-          meson
-          ninja
-          pkg-config
-        ];
+          nativeBuildInputs = [
+            meson
+            ninja
+            pkg-config
+          ];
 
-        buildInputs = [ libedit ];
+          buildInputs = [ libedit ];
 
-        meta = with lib; {
-          description = "small personal shell";
-          homepage = "https://qirus.net";
-          license = licenses.bsd3;
-          maintainers = with maintainers; [ hjones2199 ];
+          meta = with lib; {
+            description = "small personal shell";
+            homepage = "https://qirus.net";
+            maintainers = with maintainers; [ hjones2199 ];
+            license = licenses.bsd3;
+            platforms = platforms.linux;
+          };
         };
-      };
+    riscvPackage =
+      with import nixpkgs { system = "x86_64-linux"; crossSystem = nixpkgs.lib.systems.examples.riscv64; };
+        stdenv.mkDerivation rec {
+          pname = "ush";
+          version = "0.72";
+
+          src = self;
+
+          nativeBuildInputs = [
+            meson
+            ninja
+            pkg-config
+          ];
+
+          buildInputs = [ libedit ];
+
+          meta = with lib; {
+            description = "small personal shell";
+            homepage = "https://qirus.net";
+            maintainers = with maintainers; [ hjones2199 ];
+            license = licenses.bsd3;
+            platforms = platforms.linux;
+          };
+        };
   };
 }
